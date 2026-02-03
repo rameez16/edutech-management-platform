bcbad9b1915487f79263eb7f357c61e49a133196
anwar <anwarnsyd@gmail.com>
2026-02-03 14:39:27 +0530

    WIP: progress on batch overview and  students

--- apps/trainer/urls.py
+++ apps/trainer/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("profile/", views.trainer_profile, name="profile"),
     path("mybatches/", views.my_batches, name="my_batches"),
+    path("batches/<int:batch_id>/overview/", views.batch_overview_view, name="batch-overview"),
+    path("batches/<int:batch_id>/students/", views.batch_students_view, name="batch-students"),
+    
+    path(
+    "batches/<int:batch_id>/students/<int:student_id>/",
+    views.student_detail_view,
+    name="student-detail",
+),
+
 ]
