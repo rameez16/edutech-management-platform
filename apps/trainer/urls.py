739a2acd65f02d4951dbb472321a18a9ed1b2783
anwar <anwarnsyd@gmail.com>
2026-02-12 15:07:41 +0530

    Add Profile Section and Impliment Lesson plan workflow

--- apps/trainer/urls.py
+++ apps/trainer/urls.py
@@ -8,13 +8,17 @@ urlpatterns = [
     # Dashboard
     path("trainer_dashboard", views.dashboard, name="dashboard"),
     
-    # my_batches
-    path("mybatches/", views.my_batches, name="my_batches"),
-    path("batches/<int:batch_id>/overview/", views.batch_overview_view, name="batch-overview"),
-    path("batches/<int:batch_id>/students/", views.batch_students_view, name="batch-students"),
-    path("batches/<int:batch_id>/students/<int:student_id>/",views.student_detail_view,name="student-detail",),
-
     #my_profile
     path("profile/", views.profile_view, name="profile"),
     path("profile/edit/", views.profile_edit, name="profile-edit"),
+    
+    # my_batches
+    path("mybatches/", views.my_batches, name="batches"),
+    path("batches/<int:batch_id>/overview/", views.batch_overview_view, name="batch-overview"),
+    path("batches/<int:batch_id>/students/", views.batch_students_view, name="batch-students"),
+    path("batches/<int:batch_id>/students/<int:student_id>/",views.student_details_view,name="batch-student-details"),
+    path('batch/<int:batch_id>/lesson-plan/',views.batch_lesson_plan,name='batch-lesson-plan'),
+    path("batches/<int:batch_id>/feedback/",views.batch_feedback_view,name="batch-feedback"),
+
+
 ]
