575f778da947b53ec97c0e96f478b9e4ad490fd6
Rameees <rameezqux@gmail.com>
2026-02-23 21:03:17 +0530

    bug fix id card

--- apps/bdm/views.py
+++ apps/bdm/views.py
@@ -417,6 +417,7 @@ def approve_enrollment_agreement(request, student_id):
         
         checklist.id_card_generated = True
         checklist.id_card_issued = True
+        checklist.save()
         
         # ✅ AUTO CREATE LMS ACCESS (if not exists)
         lms_access, created = LMSAccess.objects.get_or_create(
