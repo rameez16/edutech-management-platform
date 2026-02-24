3f7e8ae58936b353fc2ed9d2ebb795f6e23303b5
ThresiaAleena <aleenathresia384@gmail.com>
2026-02-24 12:10:34 +0530

    admin role set

--- apps/bdm/views.py
+++ apps/bdm/views.py
@@ -59,7 +59,7 @@ from .utils import role_required
 
 
 @login_required
-# @role_required('admin')
+@role_required('admin')
 def dashboard(request):
     """
     Dashboard view with comprehensive statistics and latest data
