1127455878691a3b4d24ce4bdad7fb8dfd396125
niranjanaks012 <niranjanaks021@gmail.com>
2026-03-03 22:04:24 +0530

    conflict resolved

--- apps/bdm/admin.py
+++ apps/bdm/admin.py
@@ -26,8 +26,5 @@ admin.site.register(Notification)
 admin.site.register(StudentIssue)
 admin.site.register(Announcement)
 admin.site.register(Admission)
-<<<<<<< HEAD
-=======
 
 
->>>>>>> 82e73366d11bc7ae1f59bb22f762453491f071be
