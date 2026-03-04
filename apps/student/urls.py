96517e58cba91e564bc30128d72d7cbe56cc4f36
kandianrinta <kandianrinta@gmail.com>
2026-03-04 21:43:21 +0530

    some payment related functions,html,css removed

--- apps/student/urls.py
+++ apps/student/urls.py
@@ -5,20 +5,10 @@ app_name='student'
 
 #rinta
 urlpatterns = [path('stud_dashboard/',views.dashboard,name='stud_dashboard'),
-               path('payment/',views.payment,name='payment'),
-               path('pdc/',views.pdc,name='pdc'),
-               path('emi/', views.emi, name='emi'),
-               path("installments/",views.installments, name="installments"),
-               path('onetime_view/', views.onetime_view, name='onetime_view'),
                path('stud_profile/', views.stud_profile, name='stud_profile'),
                path('overview/', views.overview, name='overview'),
                path('password/', views.password, name='password'),
-               path('QR_pay/', views.QR_pay, name='QR_pay'),
-               path('installment/qr/<int:installment_number>/',views.install_qr,name='install_qr'),
-               
-               
                path('stud_feedback/', views.stud_feedback, name='stud_feedback'),
-               path("fees/", views.payment_gateway, name="payment_gateway"),
                path("student_attendance/", views.student_attendance, name="student_attendance"),
                path("student_leave/", views.student_leave, name="student_leave"),
                path("student_evaluation/",views.student_evaluation, name="student_evaluation"),
